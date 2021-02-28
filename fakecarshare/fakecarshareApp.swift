db5ac76b481ab50104c5e66fec84a3ff085bb07b
ohioshirt <g.ohioshirt@gmail.com>
2021-02-28 10:59:26 +0900

    Initial Commit

---
+++ fakecarshare/fakecarshareApp.swift
@@ -0,0 +1,17 @@
+//
+//  fakecarshareApp.swift
+//  fakecarshare
+//
+//  Created by shigeo on 2021/02/28.
+//
+
+import SwiftUI
+
+@main
+struct fakecarshareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
